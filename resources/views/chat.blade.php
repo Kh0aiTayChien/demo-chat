fb858f2a0f879ba086f088a0e1929c2c4126edc2
chien-le window <chien.hcckt@gmail.com>
2023-07-27 15:41:01 +0700

    chang url

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -78,7 +78,7 @@
 <script>
     $(function () {
 
-        const socket = io('http://chat-demo.27-71-27-180.flashvps.xyz/socket.io/', {
+        const socket = io('/socket.io/', {
             transports: ['websocket']
         });
 
